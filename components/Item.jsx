208aa0ef91b5b5885fe01215ac65fb0d524ad636
ChristopherHernandez117 <105824044+Thunder117@users.noreply.github.com>
2024-04-21 14:07:58 -0700

    created styles.js and changed touchableOpacities to pressables

--- components/Item.jsx
+++ components/Item.jsx
@@ -1,24 +1,25 @@
 import { Swipeable } from "react-native-gesture-handler";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, Pressable, Button, View } from "react-native";
 import { Feather } from '@expo/vector-icons';
+import styles from "./styles";
 
 const Item = (props) => {
 
     const rightSwipe = () => {
         return (
-            <TouchableOpacity style={{width:'30%', alignItems:'center', justifyContent:'center', height:'100%', marginLeft:'-10%', borderTopRightRadius:20, borderBottomRightRadius:20, marginRight:'5%', backgroundColor:'#f93737'}}>
+            <Pressable style={{width:'30%', alignItems:'center', justifyContent:'center', height:'100%', marginLeft:'-10%', borderTopRightRadius:20, borderBottomRightRadius:20, marginRight:'5%', backgroundColor:'#f93737'}}>
                 <Feather name="trash-2" size={32} color="white" />
-            </TouchableOpacity>
+            </Pressable>
         );
     };
 
     return(
-        <View style={{width:'100%', height:80, marginTop:10}}>
+        <View style={styles.itemContainer}>
 
             <Swipeable overshootRight={false} renderRightActions={rightSwipe}>
-                <View style={{backgroundColor:"#f5f5f6", width:"90%", borderRadius:20,  height:'100%', alignSelf:'center', justifyContent:"center", alignItems:"center"}}>
-                    <Text style={{fontSize:20, fontWeight:'500'}}>{props.item}</Text>
-                </View>
+                <Pressable style={styles.itemButton} onPress={() => { console.log('pressed') }}>
+                    <Text style={styles.itemText}>{props.item}</Text>
+                </Pressable>
             </Swipeable>
 
         </View>
---
+++ components/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+
+const styles = StyleSheet.create({
+    // Item
+    itemContainer: {
+        width:'100%', 
+        height:80, 
+        marginTop:10
+    },
+    itemButton: {
+        backgroundColor:"#f5f5f6", 
+        width:"90%", 
+        borderRadius:20,  
+        height:'100%', 
+        alignSelf:'center', 
+        justifyContent:"center", 
+        alignItems:"center"
+    },
+    itemText: {
+        fontSize:20, 
+        fontWeight:'500'
+    }
+});
+
+export default styles;
