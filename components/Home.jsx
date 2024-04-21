a27eb810e355100b181bde1906d590f4c0846db2
ChristopherHernandez117 <105824044+Thunder117@users.noreply.github.com>
2024-04-20 20:38:10 -0700

    improved the slider

--- components/Home.jsx
+++ components/Home.jsx
@@ -5,17 +5,20 @@ import { Feather } from '@expo/vector-icons';
 const Home = () => {
     const data = [
         {
-            name: "Bananas"
+            name: "Banano"
         },
         {
-            name: "Manzanas"
+            name: "Manzana"
+        },
+        {
+            name: "Pepino"
         }
     ];
 
     const rightSwipe = () => {
         return (
             
-            <TouchableOpacity style={{width:'30%', alignItems:'center', justifyContent:'center', height:'100%', borderRadius:30}}>
+            <TouchableOpacity style={{width:'35%', alignItems:'center', justifyContent:'center', height:'100%', marginLeft:-35, borderTopRightRadius:20, borderBottomRightRadius:20, marginRight:20, backgroundColor:'#f93737'}}>
                 <Feather name="trash-2" size={32} color="white" />
             </TouchableOpacity>
                  
@@ -25,11 +28,11 @@ const Home = () => {
     const Item = (props) => {
 
         return(
-            <View style={{width:"100%", height:80, marginTop:10, backgroundColor:'#f93737', borderRadius:30}}>
+            <View style={{width:'100%', height:80, marginTop:10}}>
 
-                <Swipeable renderRightActions={rightSwipe}>
-                    <View style={{backgroundColor:"#f5f5f6", width:"100%", borderRadius:30,  height:'100%', alignSelf:'center', justifyContent:"center", alignItems:"center"}}>
-                        <Text style={{fontSize:20}}>{props.item}</Text>
+                <Swipeable overshootRight={false} renderRightActions={rightSwipe}>
+                    <View style={{backgroundColor:"#f5f5f6", width:"90%", borderRadius:20,  height:'100%', alignSelf:'center', justifyContent:"center", alignItems:"center"}}>
+                        <Text style={{fontSize:20, fontWeight:'500'}}>{props.item}</Text>
                     </View>
                 </Swipeable>
 
@@ -38,15 +41,18 @@ const Home = () => {
     }
 
     return(
-            <View style={{flex: 1, backgroundColor:'white'}}>
-                <FlatList
-                    data={data}
-                    renderItem={({item, index}) => {
-                        return <Item item={item.name} index={index} />;
-                    }}
-                />
-            </View>
+        <View style={{flex: 1, backgroundColor:'white'}}>
+            <FlatList
+                data={data}
+                renderItem={({item, index}) => {
+                    return <Item item={item.name} index={index} />;
+                }}
+            />
+        </View>
     );
 }
 
+/*
+<View style={{width:"90%", height:'100%', marginTop:10, backgroundColor:'#f93737', borderRadius:50, alignSelf:'center'}}/>
+*/
 export default Home;
